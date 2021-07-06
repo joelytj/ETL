186273a4bde142bd2a6a2207cf559fe2fef67fc5
Joel Yang <joelytj@gmail.com>
2021-07-06 16:19:01 +0800

    Compiles assets for crowdsale for GitHub Pages

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -8,7 +8,7 @@ module.exports = function(deployer) {
   deployer.deploy(QuestionFactory, {overwrite: false});
   deployer.deploy(contractB, {overwrite: false}); 
   
-  deployer.deploy(ETLToken, 1000000000, {overwrite: false}).then(function() { //
+  deployer.deploy(ETLToken, 1000000000).then(function() { //
       // Token price is 0.1 Ether
       var tokenPrice = 1000000000000000; //original: 1000000000000000 (Token price is 0.001 Ether)
       return deployer.deploy(ETLTokenSale, ETLToken.address, tokenPrice);  //
