82b6fdd6f8bd46440539819d4c63d7adbcc11536
Joel Yang <joelytj@gmail.com>
2021-10-22 11:54:00 +0800

    Upload image to s3 with metadata address

---
+++ pages/upload/index.js
@@ -0,0 +1,29 @@
+// import React, { Component } from 'react';
+// import Upload from './upload';
+// import Layout from '../../components/Layout';
+
+// class UploadImage extends Component {
+//     render() {
+//         return (
+//             <Layout>
+//                 <Upload>
+//                 </Upload>
+//             </Layout>
+//         )
+//     }
+// }
+
+// export default UploadImage;
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
